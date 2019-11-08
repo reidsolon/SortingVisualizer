2861e9b28b3aa10fb9b2d19a5e3e6ae44fe16c26
reidsolon <rayanthonysolon@gmail.com>
2019-11-08 16:04:25 +0800

    added selection sort

--- js_sorts/js/index.js
+++ js_sorts/js/index.js
@@ -24,6 +24,7 @@ var tick = 0;
 var total_loops = 0, temp_ticks = 0, temp_loops;
 var interval = null;
 var bubbleInterval = null;
+var selectionInterval = null;
 var sortedInterval = null;
 
 //info tracker declarations
@@ -32,7 +33,7 @@ var sort_speed = document.getElementById('sort-speed');
 var condition = document.getElementById('condition');
 var view_loops = document.getElementById('total-loops');
 var total_tick = document.getElementById('total-ticks');
-var status = document.getElementById('status');
+var status_txt = document.getElementById('status');
 
 //active all buttons
 const activeButtons = () => {
@@ -66,6 +67,7 @@ const generateRandom = (_LENGTH, _NUM) => {
 
     clearInterval(interval);
     clearInterval(bubbleInterval);
+    clearInterval(selectionInterval);
     total_loops = 0;
     view_loops.innerHTML = 0;
     temp_ticks = 0;
@@ -77,13 +79,13 @@ const generateRandom = (_LENGTH, _NUM) => {
     var html = '';
     var arr_html = '';
     for( var i = 0; i < parseInt(_NUM) ; i ++ ) {
-        var new_array = Math.floor((Math.random() * _LENGTH) + 1);
+        var new_array = Math.floor((Math.random() * _LENGTH) + 20);
         if (array.includes(new_array)) {
-            new_array = Math.floor((Math.random() * _LENGTH) + 1);
+            new_array = Math.floor((Math.random() * _LENGTH) + 20);
         }
 
         array[i] = new_array;
-        html += '<div class="num animate bar_' + array[i] + '" id="bar_' + array[i] + '" style="height: ' + array[i] + 'px; background-color:#6399F1; width: '+ (80-(sort_size.value/3))+'px;">&nbsp;</div>';
+        html += '<div class="num animate bar_' + array[i] + '" id="bar_' + array[i] + '" style="height: ' + array[i] + 'px; background-color:#6399F1; width: 3px;">&nbsp;</div>';
         arr_html += array[i]+' ';
 
     }
@@ -101,16 +103,14 @@ generateRandom(sort_size.value, sort_size.value / 4);
 const bubbleSortAlgo = () => {
     disableButtons();
     var i = 0, count = 1, tick = 0;
-
-    
         bubbleInterval = setInterval(() => {
             if (!isSorted) {
-                status.innerHTML = 'Sorting..';
+                status_txt.innerHTML = 'Sorting..';
                 total_tick.innerHTML = temp_ticks;
 
                 _doAnimation(array[i], array[count]);
                 if (array[i] > array[count]) {
-                    _doSwapAnimation(array[i], array[count]);
+                    _doAnimation(array[i], array[count]);
                     var _temp = array[i];
                     array[i] = array[count];
                     array[count] = _temp;
@@ -126,9 +126,10 @@ const bubbleSortAlgo = () => {
                     total_loops = 0;
                     isSorted = true;
                     activeButtons();
-                    status.innerHTML = 'Sorted';
+                    status_txt.innerHTML = 'Sorted';
+
                     sortedAnimation();
-                    clearInterval(sortedInterval);
+                    
                 }
 
                 count++;
@@ -141,7 +142,7 @@ const bubbleSortAlgo = () => {
                     total_loops++;
                     view_loops.innerHTML = total_loops;
                     clearInterval(bubbleInterval);
-                    sorted_array.push(array[i]);
+                    sorted_array.unshift(array[i]);
                     console.log(sorted_array);
                     bubbleSortAlgo();
                 }
@@ -190,13 +191,145 @@ const bubbleSortAlgo = () => {
         }
        
 }
+const mergeSortAlgo = () => {
 
+    nums = array;
+
+    // const mergeSort = (nums) => {
+    //     const sortedArray = nums;
+    //     if (sortedArray.length < 2) {
+    //         return nums;
+    //     }
+    //     const length = sortedArray.length;
+    //     const middle = Math.floor(length / 2);
+    //     const left = sortedArray.slice(0, middle);
+    //     const right = sortedArray.slice(middle);
+
+    //     return merge(mergeSort(left), mergeSort(right));
+    // };
+
+    // const merge = (left, right) => {
+    //     const results = [];
+    //     while (left.length && right.length) {
+    //         if (left[0] <= right[0]) {
+    //             results.push(left.shift());
+    //         }
+    //         else {
+    //             results.push(right.shift());
+    //         }
+    //     }
+    //     return results.concat(left, right);
+    // };
+
+    const emptyArray = [];
+
+    const mergeSort = ()=> {
+        
+    }
+
+    const merge = ()=>{ 
+
+    }
+
+    alert(mergeSort(nums));
+
+}
+
+const selectionSortAlgo = () => {
+    disableButtons();
+    var startIdx = 0;
+    var currentMinimum = array[startIdx];
+    var currentMinimumIdx = 0;
+    var loop_counter = 0;
+    var needSwap = false;
+
+    var i = 1;
+    selectionInterval = setInterval(() => {
+        if( i != (array.length) - loop_counter) {
+
+            _doAnimation(currentMinimum, array[startIdx+i]);
+            if (array[startIdx+i] < currentMinimum) {
+                currentMinimum = array[startIdx+i];
+                currentMinimumIdx = startIdx+i;
+
+                console.log('New minimum:'+ currentMinimum+" "+'NewMinimumIdx: '+currentMinimumIdx);
+
+                _doAnimation(currentMinimum, array[startIdx+i]);
+
+                needSwap = true;
+            }
+
+            i++;   
+
+        } else{
+            if(needSwap) {
+                _doSwapAnimation(currentMinimumIdx, startIdx);
+                needSwap = false;
+            } else {
+
+                sorted_array.push(currentMinimum);
+            }
+            
+            
+
+            i = 1;
+            startIdx++;
+            loop_counter++;
+
+
+            currentMinimum = array[startIdx];
+        }
+        
+        if (loop_counter+2 == array.length) {
+
+            clearInterval(selectionInterval);
+            activeButtons();
+            sortedAnimation();
+
+        }
+
+    }, 550 - sort_size.value);
+
+    function _doAnimation(minimum, currentElement) {
+        
+        document.getElementsByClassName('bar_' + minimum)[0].style.backgroundColor = '#6CDB7B';
+        document.getElementsByClassName('bar_' + currentElement)[0].style.backgroundColor = '#DD5C5C';
+        
+        setTimeout(() => {
+            refreshBars();
+        }, 60);
+    }
+
+    function _doSwapAnimation(currentMinimumIdx, startIdx) {
+        
+        setTimeout(()=> {
+            document.getElementsByClassName('bar_' + array[currentMinimumIdx])[0].style.backgroundColor = '#6CDB7B';
+            document.getElementsByClassName('bar_' + array[startIdx])[0].style.backgroundColor = '#6CDB7B';
+        }, 20);
+
+        setTimeout(() => {
+            var _temp = array[startIdx];
+            var sortedValue = array[startIdx] = array[currentMinimumIdx];
+            array[currentMinimumIdx] = _temp;
+
+            sorted_array.push(sortedValue);
+
+            console.log(sortedValue);
+            console.log(sorted_array);
+        }, 40);
+
+        setTimeout(() => {
+            refreshBars();
+        }, 50);
+    }
+
+}
  
 const refreshBars = () => {
     var html = '';
     var arr_html = '';
     for(var i = 0; i<array.length; i++){
-        html += '<div class="num bar_' + array[i] + '" id="bar_' + array[i] + '" style="height: ' + array[i] + 'px; background-color: #6399F1;width: ' + (80 - (sort_size.value / 3)) +'px;">&nbsp;</div>';
+        html += '<div class="num bar_' + array[i] + '" id="bar_' + array[i] + '" style="height: ' + array[i] + 'px; background-color: #6399F1;width: 3px;">&nbsp;</div>';
         arr_html += array[i] + ' ';
     }
     
@@ -216,18 +349,12 @@ const sortedAnimation = () => {
         if(i<array.length){
             bars_class[i].style.backgroundColor = '#6CDB7B';
             i++;
+        } else {
+          clearInterval(sortedInterval);
+          refreshBars();
         }
         
-    }, 30);
-
-    i=0;
-
-
-    setTimeout(() => {
-            bars_class[i].style.backgroundColor = '#B578E8';
-        
-    }, 50);
-    
+    }, 50);    
 }
 
 
